a6b0552884e5f919c5da2604a8495022df96a9f0
Jerry Thomas <Jerry@Seneca-Macbook.local>
2018-06-22 18:00:57 +0530

    Ruby script to install prezto globally

---
+++ prezto.rb
@@ -0,0 +1,8 @@
+cask 'prezto' do
+    version '1.0'
+    url "https://github.com/sorin-ionescu/prezto.git"
+    name 'Prezto'
+    homepage "https://github.com/sorin-ionescu/prezto"
+  
+    binary "#{prefix}"
+  end

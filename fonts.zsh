cb09791c040c5b575668949ed799fd3b14044545
Jerry Thomas <me@jerrythomas.name>
2018-07-02 17:23:39 +0530

    added dock settings

--- fonts.zsh
+++ fonts.zsh
@@ -6,3 +6,6 @@ brew cask install font-firacode-nerd-font
 
 # Cursive font Script 12 BT
 brew cask install ./font-script12-bt.rb
+osascript -e "tell application \"Terminal\" to set default settings to settings set \"Dark Clarity\""
+osascript -e "tell application \"Terminal\" to set the font name of window 1 to \"FuraCode Nerd Font\""
+osascript -e "tell application \"Terminal\" to set the font size of window 1 to 14"

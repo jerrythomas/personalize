6af237e9ffd170a276e5887cbdce7f834044b39d
Jerry Thomas <me@jerrythomas.name>
2020-07-22 20:47:48 +0530

    switched to yarn and changed history command.

--- node.zsh
+++ node.zsh
@@ -2,6 +2,11 @@
 # Node libraries
 
 brew install node
-npm install -g @angular/cli
-npm install -g npm-check-updates
+# npm install -g npm-check-updates
 
+yarn global add electron-builder
+yarn global add eslint typescript firebase-tools
+yarn global add fastify-cli
+yarn global add @vue/cli
+yarn global add @vue/cli-init
+yarn global add angular @angular/cli
